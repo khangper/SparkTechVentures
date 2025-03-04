a53896008f3094267aedf172ee253894433919ff
duyluongg <147598253+duyluongg@users.noreply.github.com>
2025-03-04 16:20:32 +0700

    fix homepage

--- src/Context/api.js
+++ src/Context/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://constructionequipmentrental-f7d4esd5a9g4gjfv.southeastasia-01.azurewebsites.net/api",
+  baseURL: "https://sparktechventures-b7dhckb8bsgefbb9.southeastasia-01.azurewebsites.net/",
   timeout: 500000,
 });
 
